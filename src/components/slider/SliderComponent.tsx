66b731c1d6db3f44dde7f979e2ab3b0fa66f4697
Bishop Walker <bishop.walker@sbd2.com>
2023-02-04 18:46:55 -0500

    Deez Commits

--- src/components/slider/SliderComponent.tsx
+++ src/components/slider/SliderComponent.tsx
@@ -17,34 +17,31 @@ const SliderComponent = () => {
 
                 <div>
                     <h3>1</h3>
-                    <img src='src/assets/compactor_image.jpeg' style={{width: '100vw' , height: '50vh'}} />
+                    <img src='src/assets/04_16ft_14k_gooseneck_dump.jpg' style={{width: '100vw' , height: '50vh'}} />
                 </div>
                 <div>
                     <h3>2</h3>
-                    <img src='src/assets/compactor_image.jpeg' style={{width: '100vw' , height: '50vh'}} />
+                    <img src='src/assets/maxresdefault.jpg' style={{width: '100vw' , height: '50vh'}} />
                 </div>
                 <div>
                     <h3>3</h3>
-                    <img src='src/assets/compactor_image.jpeg' style={{width: '100vw' , height: '50vh'}} />
+                    <img src='src/assets/NewGooseneckProPactor-500x206.png' style={{width: '100vw' , height: '50vh'}} />
                 </div>
                 <div>
                     <h3>4</h3>
-                    <img src='src/assets/compactor_image.jpeg' style={{width: '100vw' , height: '50vh'}} />
+                    <img src='src/assets/bttw.jpg' style={{width: '100vw' , height: '50vh'}} />
 
                 </div>
                 <div>
                     <h3>5</h3>
-                    <img src='src/assets/compactor_image.jpeg' style={{width: '100vw' , height: '50vh'}} />
+                    <img src='src/assets/ProPactor_NEW19-1024x471.png' style={{width: '100vw' , height: '50vh'}} />
 
                 </div>
-                <div>
-                    <h3>6</h3>
-                    <img src='src/assets/compactor_image.jpeg' style={{width: '100vw' , height: '50vh'}} />
 
-                </div>
             </Slider>
         </div>
     )
 }
 
+// @ts-ignore
 export default SliderComponent
